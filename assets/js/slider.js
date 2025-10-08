41ce19bb51ecbed5354a4b9d654a93dd0fb10d24
KadochnikovK <kadochnikov_k@mail.ru>
2025-10-08 15:38:47 +0500

    added advantages slider
    Signed-off-by: KadochnikovK <kadochnikov_k@mail.ru>

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
     spaceBetween: 20,
     centeredSlides: false,
     loop: true,
-    
+
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
@@ -171,5 +171,77 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       },
     },
+  });
+
+  const advantagesSwiper = new Swiper(".advantages__slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+
+
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+
+    
+    simulateTouch: true,
+    grabCursor: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    allowTouchMove: true, 
+
+
+    effect: "slide",
+    speed: 800,
+    threshold: 5, 
+
+
+    longSwipes: true,
+    longSwipesRatio: 0.5,
+    longSwipesMs: 300,
+
+
+    shortSwipes: true,
+    followFinger: true,
+
+
+    preventInteractionOnTransition: false,
+    resistance: true,
+    resistanceRatio: 0.6,
+
+    on: {
+      init: function () {
+     
+        this.addLineClickHandlers();
+      },
+      slideChange: function () {
+        console.log("Текущий слайд:", this.activeIndex);
+      },
+    },
+
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+    },
+  });
+
+  document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("advantages__line")) {
+      console.log("Следующий слайд");
+      advantagesSwiper.slideNext();
+    }
   });
 });
