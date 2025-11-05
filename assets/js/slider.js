cd3a6bb72ae75f5a6adfa12b8e6e3802e4a17203
KadochnikovK <kadochnikov_k@mail.ru>
2025-11-05 22:32:12 +0500

    added extra sections
    Signed-off-by: KadochnikovK <kadochnikov_k@mail.ru>

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -59,6 +59,81 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
+  const scheduleSwiper = new Swiper(".schedule__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    centeredSlides: false,
+    loop: true,
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+    simulateTouch: true,
+    grabCursor: true,
+
+    touchRatio: 0.6,
+    touchAngle: 45,
+
+    freeMode: {
+      enabled: true,
+      momentum: true,
+      momentumRatio: 2,
+      momentumBounce: true,
+      momentumBounceRatio: 1,
+      momentumVelocityRatio: 2,
+      sticky: true,
+    },
+
+    speed: 600,
+    resistanceRatio: 0.6,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1.05,
+        freeMode: {
+          enabled: true,
+          momentum: true,
+          momentumRatio: 3,
+          sticky: true,
+        },
+        speed: 800,
+      },
+      640: {
+        slidesPerView: 2,
+        freeMode: {
+          enabled: true,
+          momentum: true,
+          momentumRatio: 2,
+        },
+      },
+      1000: {
+        slidesPerView: 3,
+        freeMode: {
+          enabled: true,
+          momentum: true,
+          momentumRatio: 2,
+        },
+      },
+      1300: {
+        slidesPerView: 4,
+        freeMode: {
+          enabled: true,
+          momentum: true,
+          momentumRatio: 2,
+        },
+      },
+      1500: {
+        slidesPerView: 5,
+        freeMode: {
+          enabled: true,
+          momentum: true,
+          momentumRatio: 2,
+        },
+      },
+    },
+  });
   console.log("2: перед partnersSwiper");
   const partnersSwiper = new Swiper(".partners__slider", {
     slidesPerView: "auto",
